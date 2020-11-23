3f292372788f5363881c146db57da3cf470faeca
Fernando Junior <32521805+FernandoTHE@users.noreply.github.com>
2020-11-23 11:40:54 -0300

    Add files via upload

--- components/CustomTab.js
+++ components/CustomTab.js
@@ -8,38 +8,40 @@ import RotasIcon from '../assents/rotas.svg';
 import ValidaIcon from '../assents/validação.svg';
 
 const TabArea = styled.View`
-  height: 110px;
-  background-color: #ffffff;
+  height: 70px;
+  background-color: #fff;
   flex-direction: row;
+  /* width: 100%; */
 `;
 
 const TabItem = styled.TouchableOpacity`
   flex: 1px;
   justify-content: center;
+  /* width: 100%; */
   align-items: center;
 `;
 
 export default () => {
   return (
     <TabArea>
-      <TabItem>
-        <HomeIcon width="30" heigth="30" fill="#FFFFFF" />
+      <TabItem onPress={() => goTo('Home')}>
+        <HomeIcon width={30} heigth={30} fill="#FFFFFF" />
       </TabItem>
 
       <TabItem>
-        <PerfilIcon width="30" heigth="30" fill="#FFFFFF" />
+        <PerfilIcon width={30} heigth={30} fill="#FFFFFF" />
+      </TabItem>
+
+ \   q     {/* <TabItem>
+        <ProcurarIcon width={30} heigth={30} fill="#FFFFFF" />
+      </TabItem> */}
+
+      <TabItem>
+        <RotasIcon width={30} heigth={30} fill="#FFFFFF" />
       </TabItem>
 
       <TabItem>
-        <ProcurarIcon width="30" heigth="30" fill="#FFFFFF" />
-      </TabItem>
-
-      <TabItem>
-        <RotasIcon width="30" heigth="30" fill="#FFFFFF" />
-      </TabItem>
-
-      <TabItem>
-        <ValidaIcon width="30" heigth="30" fill="#FFFFFF" />
+        <ValidaIcon width={30} heigth={30} fill="#FFFFFF" />
       </TabItem>
     </TabArea>
   );
